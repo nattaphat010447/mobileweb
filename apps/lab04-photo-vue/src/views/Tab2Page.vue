c1abbe7dfd9efecbccc8aaa12cb7f92817013ef1
nattaphat010447 <jangkup90@gmail.com>
2026-01-11 21:57:46 +0700

    fix: update Vite version in package.json and package-lock.json; enhance Tab2Page layout with photo grid

--- apps/lab04-photo-vue/src/views/Tab2Page.vue
+++ apps/lab04-photo-vue/src/views/Tab2Page.vue
@@ -8,22 +8,27 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Photo Gallery นายณัฐพัฒน์ แสนตรี 663380012-6</ion-title>
+          <ion-title size="large">Photo Gallery</ion-title>
         </ion-toolbar>
       </ion-header>
 
-      <!-- CHANGE: Add the floating action button -->
       <div class="ion-padding">
         <strong>โดย นายณัฐพัฒน์ แสนตรี รหัส 663380012-6</strong>
       </div>
+
+      <ion-grid>
+        <ion-row>
+          <ion-col size="6" :key="photo.filepath" v-for="photo in photos">
+            <ion-img :src="photo.webviewPath"></ion-img>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+
       <ion-fab vertical="bottom" horizontal="center" slot="fixed">
         <ion-fab-button @click="addNewToGallery">
           <ion-icon :icon="camera"></ion-icon>
         </ion-fab-button>
       </ion-fab>
-
-      <!-- CHANGE: Remove or comment out <ExploreContainer /> -->
-      <!-- <ExploreContainer name="Tab 2 page" /> -->
     </ion-content>
   </ion-page>
 </template>
@@ -33,5 +38,5 @@ import { camera } from 'ionicons/icons';
 import { IonPage, IonHeader, IonFab, IonFabButton, IonIcon, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonImg } from '@ionic/vue';
 import { usePhotoGallery } from '@/composables/usePhotoGallery';
 
-const { addNewToGallery } = usePhotoGallery();
+const { photos, addNewToGallery } = usePhotoGallery();
 </script>
